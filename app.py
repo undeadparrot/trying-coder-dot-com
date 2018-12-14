6e6a3171dca5b06a6edf711538851b2b5fbf0e20
undeadparrot <smatuszeksa@gmail.com>
2018-12-14 12:24:44 +0000

    blah

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 def add(x:int, y:int) -> int:
     return x+y
 
+def hello_renier():
+    print("HELLO RENIER!")
+
+# Lets put in some comments
 if __name__ == '__main__':
-    print(add(1,2))
+    hello_renier()
